3c4f677df651b84998bf3bc14f8df040e6b08df3
Aftab Uddin <aftabbyofficial@gmail.com>
2023-12-31 17:17:43 +0600

    Some changes

--- Python Language A-Z/Harvard CS50/OOP-1.py
+++ Python Language A-Z/Harvard CS50/OOP-1.py
@@ -1,16 +1,34 @@
+'''     Till 11:28:00     '''
+
 class Student:
     def __init__(self, name, house): #instance method, it initializes the object while creating
-        self.name = name
-        self.house = house
+        if not name:
+            raise ValueError("Name Missing.")   #--raise -- is just another kind of KW to raise an error, so that you can later try & except handle it.
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
 
+        self.name = name    #Here -- self -- is the object that just created
+        self.house = house
+    
+    def __str__(self):  #A special kind of string that makes the object (while creating from the class) like an string while accessed from outside, like -- print(object_name) -- 
+        #return "a student" #Comment it off, just to get the idea
+        return f"{self.name} from {self.house}"
+
+     
+
+
+
+ 
 def main():
     student = get_student()
-    print(f"{student.name} is from {student.house}")
+    #print(f"{student.name} is from {student.house}")
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student(name, house)
+
+    student = Student(name, house)      #Constructor call to create an object called -- student -- from Student class --
     return student
 
 if __name__ == "__main__":
