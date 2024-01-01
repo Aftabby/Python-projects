effa6de3882040d607578c68fa0b07040f993371
Aftab Uddin <aftabbyofficial@gmail.com>
2024-01-01 15:33:40 +0600

    final - all revised

--- Python Language A-Z/Harvard CS50/OOP-1.py
+++ Python Language A-Z/Harvard CS50/OOP-1.py
@@ -8,23 +8,23 @@ class Student:
         self.name = name    #Here -- self -- is the object that just created
         self.house = house  #But here the property(or variable) is not the name or the house, rather it calls the name and house method, which is below, (getter/property & setter)
     
-    def __str__(self):  #A special kind of string that makes the object (while creating from the class) like an string while accessed from outside, like -- print(object_name) -- 
+    def __str__(self):  #A special kind of method that is automatically called while the object (that is made from the class) is accessed from outside, like -- print(object_name) -- 
         #return "a student" #Comment it off, just to get the idea
         return f"{self.name} from {self.house}"
 
 
     #Getter
     @property   #Is telling the python to work the below method as a getter, that is, when the variable will be called from outside(which must be the same name as the method), it will run the method and return the value from the method, that prevents direct accessing of the variable
-    def house(self):    #THough you can access the variable very easily, but the conventional way to do is - when you access or modify a variable you do it through a method
-        return self._house   #Name the method, here --- house --- exactly like the property (or variable)
+    def house(self):    #THough you can access the variable very easily (with --  self._house  -- here), but the conventional way to do is - when you access or modify a variable you do it through a method
+        return self._house   #Name the method, here --- house --- exactly like the property (or variable) -- to be clear, the variable name is _house and the getter method is house
 
 #Imp : Here actually the parameter name is ---  _house  --- , as we can't have same name for both the method and variable. but when we call object_name.house or self.house , we are actually calling either the getter or the setter (depending upon if we want to set a new value or jest get the value), and in the method the new value is assigned in the --   _house  --- variable (or property)
 
     #Setter
-    @house.setter   #It is telling the python to work the below method as a setter of the the property (or variable) which is the same name as the -- house -- variable name is _house, method name is house, and setter name is @house.setter, you can only set setter, after you set getter -- it is called python decorator. 
+    @house.setter   #It is telling the python to work the below method as a setter of the the property (or variable) which is the same name as the getter method -- house -- variable name is _house, method name is house, and setter decorator is @house.setter, you can only set setter, after you set getter -- it is called python decorator. 
     def house(self, new_house):     #THough you can access the variable very easily, but the conventional way to do is - when you access or modify a variable you do it through a method, so that we can run a block of code to check if the new value is valid or not
-        # Whenever anyone will try to change the value of the property (or variable ) -- house -- the method will automatically be called, from the main() function or the first time from __init__() method, each and every time.
-        # The second parameter ( new_house ) will be the value after the assignment operator while assigning a new value to the property(or variable) -- house --
+        # Whenever anyone will try to change the value of the property (or variable ) -- house -- the method will automatically be called, from the main() function or the first time from __init__() method from the very same class of its own, each and every time.
+        # The second parameter ( new_house ) will be the value after the assignment operator(=) while assigning a new value to the property(or variable) -- house --
         if new_house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self._house = new_house
@@ -43,7 +43,9 @@ class Student:
         self._name = new_name
 
 
+    #Class Method
     @classmethod    #This is a class method, we can call the class method without creating an object - TO know more detail about the class read the -- OOP-2.py -- in the same directory
+                    #Class method is usually used when we want to do something without or before creating the object from class, like here, we need the name and the house before creating the object, and then we return an object created with the class from this class-method
     def get(cls):
         name = input("Enter name: ")
         house = input("Enter house: ")  
@@ -57,17 +59,17 @@ class Student:
 
  
 def main():
-    #student = get_student()    #Using the function we are getting the details of the student. More conventional way to do is, all the methods, variable needed just for student should be inside a class - SO we will use a -- class method -- here
+    #student = get_student()    #Using the function we are getting the details of the student. More conventional way to do is, all the methods, variable needed for one purpose, everything relevant to that purpose should be inside a class - SO we will use a -- class method -- here
     
     #Usine a -- class method -- details is in OOP-2.py - file
-    student = Student.get()     #That's how you call class method (it is not an object method(instance method))
+    student = Student.get()     #That's how you call class method (it is not an object method(instance method)) (You don't need to create an object to call a class method, rather -- Class_name.class_method() -- )
 
     #print(f"{student.name} is from {student.house}")
 
-    #WE can change the attribute by accessing the object variable from here, but it actually triggering a method -- house -- from @house.setter -- the actual object variable is --  _house  ---
+    #WE can change the attribute by accessing the object variable ( --  _house  --) from here, but it actually triggering a method -- house -- from @house.setter -- the actual object variable is --  _house  ---
     student.house = "Slytherin"
 
-    print(student)  #When we try to access the whole object, it runs a special method from the object called __str__  ;; See the class definition above for more info
+    print(student)  #When we try to access the whole object, it runs a special method from the object called __str__  ;; See the method definition above for more info
 
 
 
--- Python Language A-Z/Harvard CS50/OOP-2 (Class Method).py
+++ Python Language A-Z/Harvard CS50/OOP-2 (Class Method).py
@@ -1,5 +1,3 @@
-
-
 import random
 
 class Hat:
@@ -14,18 +12,18 @@ class Hat:
         self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"] #It will be created when an object is created, and the __init__ function is called
     
     
-    #Class Method (@classmethod) - It is a method that is for this specific class, no matter the valur of the property of the object
-                    #It more focused on the class itself, rather than the object cretaed with itself
+    #Class Method (@classmethod) - It is a method that is for this specific class, no matter the value of the property of the object
+                    #It more focused on the class itself, rather than the object cretaed with it
                     #Class method is like class variable which is shared by all the objects of that class, not differently created for each of the object made from that class, thus takes less memory
-    @classmethod    #that's how you declare a class method
-    def sortss(cls, name):    #As it is a class method, we used -- cls -- instead of -- self --
-        houses = random.choice(cls.housesss) 
+    @classmethod    #that's how you declare a class method - with the decorator @classmethod
+    def sortss(cls, name):    #As it is a class method, we used -- cls -- instead of -- self -- because cls represent the class itself, where self represents the object itself
+        houses = random.choice(cls.housesss) #accessing a class variable, not an instance variable
         print(f"{name} is in {houses}")
 
 
     #object method (or Instance Method) #As like class method has a decorator like, @class method , instance method has no decorator - it is by default
     #def sort(self, name):    #As it is an object method, we used self
-        #house = random.choice(cls.housesss) 
+        #house = random.choice(self.houses) #To use object method(or Instance method) we need to create an object
         #print(f"{name} is in {house}")
 
 
@@ -36,5 +34,5 @@ class Hat:
 #hat.sort("Harry")
 
 
-#Class Method using - Below is the class method -- to use an class method, you don't need to create an object. It already associated with the class, so you can call it directly by calling the class
+#Class Method using - Below is the class method -- to use an class method, you don't need to create an object. It already associated with the class, so you can call it directly by calling the -- Class_name.class_method()   --
 Hat.sortss("Harry")
