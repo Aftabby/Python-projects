9840caa5645bf286c67c10ec7efce18007a71bc7
Aftab Uddin <aftabbyofficial@gmail.com>
2024-01-07 03:04:02 +0600

    with little more details in oop2

--- Python Language A-Z/Harvard CS50/OOP-2 (Class Method).py
+++ Python Language A-Z/Harvard CS50/OOP-2 (Class Method).py
@@ -8,13 +8,14 @@ class Hat:
 
     #Instance Method
     def __init__(self):
-        #Instance variable - In the
+        #Instance variable - (or you can call it object variable, though it is not what it is called)
         self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"] #It will be created when an object is created, and the __init__ function is called
     
     
     #Class Method (@classmethod) - It is a method that is for this specific class, no matter the value of the property of the object
                     #It more focused on the class itself, rather than the object cretaed with it
                     #Class method is like class variable which is shared by all the objects of that class, not differently created for each of the object made from that class, thus takes less memory
+                    # you can call the class method from outside the scope by -- class_name.class_method_name()
     @classmethod    #that's how you declare a class method - with the decorator @classmethod
     def sortss(cls, name):    #As it is a class method, we used -- cls -- instead of -- self -- because cls represent the class itself, where self represents the object itself
         houses = random.choice(cls.housesss) #accessing a class variable, not an instance variable
