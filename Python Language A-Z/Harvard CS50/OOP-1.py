2a001dc988e58e6d982212029f619f9fb4357a3a
Aftab Uddin <aftabbyofficial@gmail.com>
2024-01-07 02:58:27 +0600

    with extra detail on oop1

--- Python Language A-Z/Harvard CS50/OOP-1.py
+++ Python Language A-Z/Harvard CS50/OOP-1.py
@@ -1,24 +1,24 @@
 class Student:
-    def __init__(self, name, house): #instance method, the __init__ initializes or only called when the object is creating
+    def __init__(self, namee, housee): #instance method, the __init__ initializes or only called when the object is creating
         if not name:
             raise ValueError("Name Missing.")   #--raise -- is just another kind of KW to raise an error, so that you can later try & except handle it.
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
+            raise ValueError("Invalid house")       #Here -- Invalid house -- is just a message that you pass with the exception error
 
-        self.name = name    #Here -- self -- is the object that just created
-        self.house = house  #But here the property(or variable) is not the name or the house, rather it calls the name and house method, which is below, (getter/property & setter)
+        self.name = namee    #Here -- self -- is the object that just created
+        self.house = housee  #But here the property(or variable) is not the name or the house, rather it calls the name and house method, which is below, (getter/property & setter)
     
-    def __str__(self):  #A special kind of method that is automatically called while the object (that is made from the class) is accessed from outside, like -- print(object_name) -- 
+    def __str__(self):  #A special kind of method that is automatically called while only the object_name (that is made from the class) is called from outside this scope, like -- print(object_name) -- 
         #return "a student" #Comment it off, just to get the idea
         return f"{self.name} from {self.house}"
 
 
     #Getter
-    @property   #Is telling the python to work the below method as a getter, that is, when the variable will be called from outside(which must be the same name as the method), it will run the method and return the value from the method, that prevents direct accessing of the variable
+    @property   #Is telling the python to work the below method as a getter, that is, when the variable will be called from any scope(which must be the same name as the method), it will run the method and return the value from the method, that prevents direct accessing of the variable,
     def house(self):    #THough you can access the variable very easily (with --  self._house  -- here), but the conventional way to do is - when you access or modify a variable you do it through a method
         return self._house   #Name the method, here --- house --- exactly like the property (or variable) -- to be clear, the variable name is _house and the getter method is house
 
-#Imp : Here actually the parameter name is ---  _house  --- , as we can't have same name for both the method and variable. but when we call object_name.house or self.house , we are actually calling either the getter or the setter (depending upon if we want to set a new value or jest get the value), and in the method the new value is assigned in the --   _house  --- variable (or property)
+#Imp : Here actually the parameter name is ---  _house  --- , as we can't have same name for both the method and variable. but when we call object_name.house or self.house , we are actually calling either the getter or the setter (depending upon if we want to set a new value or jest get the value), and in the method the new value is assigned in the --   _house  --- variable (or property), just putting an underscore infront of the method to set the actual variable name is the conventiona way to set the variable
 
     #Setter
     @house.setter   #It is telling the python to work the below method as a setter of the the property (or variable) which is the same name as the getter method -- house -- variable name is _house, method name is house, and setter decorator is @house.setter, you can only set setter, after you set getter -- it is called python decorator. 
@@ -28,7 +28,7 @@ class Student:
         if new_house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self._house = new_house
-    #when someone commands -- object_name.variable_name( here variable name is house) -- the Getter or @property house method will get called
+    #when someone commands -- object_name.variable_name( here variable name is house, but the actual variable name is --   _house   --) -- the Getter or @property house method will get called
     #when someone commands -- object_name.variable_name(here it is house) = some_value -- then Setter or @house.setter method will get called with an argument of -- some_value --
 
      
@@ -46,7 +46,7 @@ class Student:
     #Class Method
     @classmethod    #This is a class method, we can call the class method without creating an object - TO know more detail about the class read the -- OOP-2.py -- in the same directory
                     #Class method is usually used when we want to do something without or before creating the object from class, like here, we need the name and the house before creating the object, and then we return an object created with the class from this class-method
-    def get(cls):
+    def get(cls):       #As this is a class method, here the first parameter represent the class itself, which is automatically passed by python when calling the class method
         name = input("Enter name: ")
         house = input("Enter house: ")  
         return cls(name,house)      #Here cls is the class itself, so it is returning an object from the very class Student itself,,, it means cls(name, house) is equals to Student(name, house)
@@ -66,7 +66,9 @@ def main():
 
     #print(f"{student.name} is from {student.house}")
 
-    #WE can change the attribute by accessing the object variable ( --  _house  --) from here, but it actually triggering a method -- house -- from @house.setter -- the actual object variable is --  _house  ---
+    #WE can change the attribute by accessing the object variable (-- house --) from here, which will not directly change the variable, It will call the method getter or setter, then the method will actually change that
+    # We can also change and access via the original variable name() --  _house  --) from here, but the conventional way is you(and all the programmers) do not touch (change) the variable which starts with an underscore (_)
+    #when we call the variable --  object_name.house -- it actually triggering a method -- house -- from @house.setter -- the actual object variable is --  _house  ---
     student.house = "Slytherin"
 
     print(student)  #When we try to access the whole object, it runs a special method from the object called __str__  ;; See the method definition above for more info
