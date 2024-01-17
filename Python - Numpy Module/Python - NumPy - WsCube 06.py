53287c62c42e36273a832ba5fef07ec59535803d
Aftab Uddin <aftabbyofficial@gmail.com>
2024-01-17 22:29:38 +0600

    edited

--- Python - Numpy Module/Python - NumPy - WsCube 06.py
+++ Python - Numpy Module/Python - NumPy - WsCube 06.py
@@ -7,15 +7,15 @@ import numpy as np
 # a/b np.divide(a,b)                # a%b np.mod(a, b)              # a**b np.power(a**b)
 # 1/a np.reciprocal(a)
 #=============================================================================================
-# Minimum Value np.min(a)           # Maximum Value np.max(a)       # Postion of Minimum Value np.argmin(a, axis=)           - Arguments of min(argmin)    N.B: Min, Max - For multidimensional array you have to pass axis(0 or 1 etc.) to find the minimum/maximum element of column or rows
-# Cosine  np.cos(a)                 # Sine      np.sin(a)           #Postion of Maximum Value  np.argmax(a, axis=)           - Arguments of max(argmax)    
-# Cumulative Sum  np.cumsum(a)      # # Square Root   np.sqrt(a)
+# Minimum Value np.min(a)           # Maximum Value np.max(a)       # Postion of Minimum Value np.argmin(a, axis=)           - Here argmin is Arguments of min      N.B: Min, Max - For multidimensional array you have to pass axis(0 or 1 etc.) to find the minimum/maximum element of column or rows
+# Cosine  np.cos(a)                 # Sine      np.sin(a)           #Postion of Maximum Value  np.argmax(a, axis=)           - Here argmax is Arguments of max    
+# Cumulative Sum  np.cumsum(a)      # Square Root   np.sqrt(a)
 #===============================================================================================
 
 
 
-#Addition - with constant and with other arrays - only the arrays of the same shapes can be added together
-            # Same procedures applies for any dimension of array
+#Addition - with constant values and with other arrays - only the arrays of the same shapes (also same dimension) can be added together with other arrays
+                    # Constant value doesn't have to be of same shape
 list1 = [1, 2, 3, 4]
 arr1 = np.array(list1)
 
@@ -31,9 +31,16 @@ print("\n\nArray 1: \n", arr1, "\nArray2: \n", arr2, "\nArray1 + Array2:\n", arr
 
 
 
+
+
+
 #Subtraction- with constant and with other arrays - only the arrays of the same shapes can be subtracted from each other
 print("Array1:\n", arr1, "\nArray1 - 3 :\n", (arr1 - 3))
 print("\n\nArray 1: \n", arr1, "\nArray2: \n", arr2, "\nArray1 - Array2:\n", (arr1 - arr2)) # # To add you can also use function to do the same task - the functions are described at the top of the file, the function returns the calculated result as an array
+
+
+
+
 
 
 
@@ -46,9 +53,17 @@ print("\n\nArray 1: \n", arr1, "\nArray2: \n", arr2, "\nArray1 * Array2:\n", (ar
 
 
 
+
+
+
+
 #Division -with constant and with other arrays - only the arrays of the same shapes can be divided by one another
 print("Array1:\n", arr1, "\nArray1 / 3 :\n", (arr1 / 3))
 print("\n\nArray 1: \n", arr1, "\nArray2: \n", arr2, "\nArray1 / Array2:\n", (arr1 / arr2)) # To add you can also use function to do the same task - the functions are described at the top of the file, the function returns the calculated result as an array
+
+
+
+
 
 
 
@@ -58,8 +73,13 @@ print("\n\nArray 1: \n", arr1, "\nArray2: \n", arr2, "\nArray2 % Array1:\n", (ar
 
 
 
-#--------------------------------------------------------------------------------
 
+
+
+
+
+#--------------------------------------------------------------------------------
+# Min, Max, Argmin, Argmax, Cumsum
 list3 = [1, 3, 6, 12, 9, 4, 2]
 arr3  = np.array(list3)
 
