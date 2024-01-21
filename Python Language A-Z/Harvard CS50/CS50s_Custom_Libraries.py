27c6bbee2ca9cfbf97bc61fccab8eec9c84bc8ca
Aftab Uddin <aftabbyofficial@gmail.com>
2024-01-22 00:10:25 +0600

    updated

--- Python Language A-Z/Harvard CS50/CS50s_Custom_Libraries.py
+++ Python Language A-Z/Harvard CS50/CS50s_Custom_Libraries.py
@@ -1,6 +1,6 @@
 def main():
     hello("World")
-    goodbye("World")
+    goodbye("Earth")
 
 def hello(name):
     print(f"hello {name}")
@@ -11,14 +11,15 @@ def goodbye(name):
 
 
 if __name__ == "__main__": #This (name) is a very special variable of python, it gets equal to "__main__", when you run the program from command line, not by calling as a library to another file
-    main()                  # And thus the code will not get trigerred while using it as a library to another file
+    main()                  # And thus the code will not get trigerred while using it as a module to another file
 
 
 
 
 # Important NOte: If you program anything outside of a function
-   # when importing the codes will run by default,
-    # Becuase at first python read the library fully so all the codes gets executed
+    # while importing the file to another file, the codes will run by default,
+    # Becuase at first python read the library/module fully so all the codes gets executed
     # To avoid this put main code in main function
     # If needed call -- main() -- from below
     # Or you can call main() the way I called inside a condition
+    # --  __name__=="main"  -- gets only True, when you run this file directly, not when you import it to another file
