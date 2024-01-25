49e2b18494c199dc6df64a8cdc7f086e01e90e32
Aftab Uddin <aftabbyofficial@gmail.com>
2024-01-25 10:13:35 +0600

    updated

--- Python - SQL - SQLite-2.py
+++ Python - SQL - SQLite-2.py
@@ -33,7 +33,7 @@ print(all_email, type(all_email))
 if user_email in all_email[0]:
     print("Congrats! You're an User!!")
 else:
-    user_password = input("You are not an user. To be a new user, please enter your new password.")
+    user_password = input("You are not an user. To be a new user, please register with a new password: ")
 
 
 
@@ -42,22 +42,22 @@ else:
 
 
 
-    #Creating a New USER           #======================  DYNAMIC QUERY  =====================================
-    next_id = id[0] + 1
-    print(next_id, type(next_id))
+#Creating a New USER           #======================  DYNAMIC QUERY  =====================================
+next_id = id[0] + 1
+print(next_id, type(next_id))
 
-    cursor.execute("INSERT INTO Users VALUES(:id, :user_email, :user_password, 0)", {"id" : next_id, "user_email" : user_email, "user_password" : user_password})              
+cursor.execute("INSERT INTO Users VALUES(:id, :user_email, :user_password, 0)", {"id" : next_id, "user_email" : user_email, "user_password" : user_password})              
                         # To put a dynamic value inside a sql query we use dynamic place holder
                         # That is, we put a --  key (of a dictionary) -- and before it we put a colon (:) , and, in the second parameter of the function cursor.execute()  we pass a dictionary with same key as we passed as a dynamic_value_placeholder in the SQL query. The value of that key from the dictionary (we passed in the second parameter) gets replaced to the SQL query
                         # We could have also used concatanation, but that will increase the chance of getting hacked
                         # You could have multiple placeholders, make sure every named parameter on your dinamic query exists as the key of the dictionary you are passing in the second parameter.
-    conn.commit()
+conn.commit()
 
 
-    #Reading the new User
-    cursor.execute("SELECT * FROM Users WHERE Id = :id", {"id" : next_id})
-    user = cursor.fetchone()      #cursor.fetchone() returns tuples
-    print("Contratulation you are an user now, here is your details: ",*user)        #unpacked tuple with an asterisk
+#Reading the new User
+cursor.execute("SELECT * FROM Users WHERE Id = :id", {"id" : next_id})
+user = cursor.fetchone()      #cursor.fetchone() returns tuples
+print("Contratulation you are an user now, here is your details: ",*user)        #unpacked tuple with an asterisk
 
 
 
@@ -81,11 +81,12 @@ else:
     
 # If you want to read only one value , no duplicate data from a column then use Distinct kw
     
-cursor.execute("SELECT DISTINCT City FROM Friends")         #Suppose you have a table named -- Friends -- and all friends has a city column
+#cursor.execute("SELECT DISTINCT City FROM Friends")         #Suppose you have a table named -- Friends -- and all friends has a city column
                                                             # Two or more friends could be from the same city
                                                             # But you wanna only know from how many cities you got friends
                                                             #Then with distinct kw , it will give you each name of the city only one time.
                                                             # You only wanna know all the cities, not wanna watch a city again and again
+                                                            #This code will give use an error as we do not have a table named -- Friends -- and no column in it named -- City --
 
 
 
