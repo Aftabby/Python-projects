a9edb6ab68f093994096c02fdddabf2294645682
Aftab Uddin <aftabbyofficial@gmail.com>
2024-01-29 05:53:11 +0600

    updated

--- Python - Custom Iterators.py
+++ Python - Custom Iterators.py
@@ -3,7 +3,7 @@
 # An object that can be iterated upon, using various types of loops in python, and will return data, one element at a time, is called iterators (example: list , tuple)
 # To make your custom iterator -- object --, you have to create two extra special methods while creating -- class -- from which you will create the object
 
-# 1. ---    __iter__()    --- Each iterator class should have the __iter__() method. It is the initialization of an iterator (Just like we use __init__() to initializa a class). It returns an iterator object
+# 1. ---    __iter__()    --- Each iterator class should have the __iter__() method. It is the initialization of an iterator (Just like we use __init__() to initialize an object). It returns an iterator object
 # 2. ---   __next__()       ----  An iterator class should have a __next__() method. This method is basically used to go on to the next element in a sequence and iterate over an iterator. It returns the next value for the iterable. 
 
 # A python iterator object must implement two special methods , __iter__(), __next() -- collectively called the iterator protocol. 
@@ -39,19 +39,19 @@ class Data:
         self.limit = limit
 
     def __iter__(self):                 # Step 2
-        self.value = 10                                     # __iter__ method returns the iterator object
+        self.value = 0                                     # __iter__ method returns the iterator object
         return self
     
     def __next__(self):                 # Step 3 
         x = self.value
-        if x > self.limit:      #When it reached the end value
+        if x >= self.limit:      #When it reached the end value
             raise StopIteration
         else:
             self.value = x+1
             return x
 
 
-for i in Data(15):
+for i in Data(10):
     print(i)
 
 # Strings, Lists, Sets, Tuples etc are built-in iterators in Python.
